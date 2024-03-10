b4545ddd3985b0003e340e9756b82245c5712b18
MrKyrylo <138040487+MrKyrylo@users.noreply.github.com>
2024-03-10 17:43:55 +0200

    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const apiKey = '42660522-ae3104db13d278f91a0482c47';
+export async function searchImages(query, page = 1, perPage = 15) {
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=${apiKey}&q=${query}&page=${page}&per_page=${perPage}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
