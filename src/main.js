a5d3a2bb36a5e585a89cc25671548a64391105cc
MrKyrylo <138040487+MrKyrylo@users.noreply.github.com>
2024-04-20 08:39:30 +0300

    1

--- src/main.js
+++ src/main.js
@@ -34,49 +34,58 @@ async function handleSearch(event) {
   searchWord = event.currentTarget.elements.inputSearch.value;
   page = 1;
 
-  try {
-    const data = await doFetch(searchWord, page);
+  doFetch(searchWord, page)
+    .then(data => {
+      if (data.total == 0) {
+        iziToast.show({
+          title: 'Ops.',
+          titleColor: 'white',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: 'white',
+          color: 'red',
+          position: 'topCenter',
+          timeout: '5000',
+        });
+        return;
+      } else {
+        photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
+        if (photoGallery.children.length) {
+          buttonMore.hidden = false;
+        }
+        book.refresh();
+        event.target.reset();
+        page = page + 1;
+        pageLimit = Math.floor(data.data.totalHits / 15);
 
-    if (data.total === 0) {
+        if (page == pageLimit) {
+          iziToast.show({
+            titleColor: 'white',
+            message: `We're sorry, but you've reached the end of search results.!`,
+            messageColor: 'white',
+            color: 'blue',
+            position: 'topCenter',
+            timeout: '5000',
+          });
+          buttonMore.hidden = true;
+        }
+      }
+    })
+    .catch(error => {
       iziToast.show({
         title: 'Ops.',
         titleColor: 'white',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
+        message: error,
         messageColor: 'white',
         color: 'red',
         position: 'topCenter',
-        timeout: 5000,
+        timeout: '5000',
       });
-      return;
-    } else {
-      photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
-
-      if (photoGallery.children.length) {
-        buttonMore.hidden = false;
-      }
-
-      book.refresh();
-      event.target.reset();
-      page = page + 1;
-      pageLimit = Math.floor(data.data.totalHits / 15);
-
-      checkShowMoreButton();
-    }
-  } catch (error) {
-    iziToast.show({
-      title: 'Ops.',
-      titleColor: 'white',
-      message: error,
-      messageColor: 'white',
-      color: 'red',
-      position: 'topCenter',
-      timeout: 5000,
+    })
+    .finally(() => {
+      loader.style.borderColor = 'white';
+      loader.style.borderBottomColor = 'transparent';
     });
-  } finally {
-    loader.style.borderColor = 'white';
-    loader.style.borderBottomColor = 'transparent';
-  }
 }
 
 async function searchMore(event) {
@@ -84,55 +93,53 @@ async function searchMore(event) {
   loader.style.borderColor = 'black';
   loader.style.borderBottomColor = 'transparent';
 
-  try {
-    const data = await doFetch(searchWord, page);
-
-    if (data.total === 0) {
+  doFetch(searchWord, page)
+    .then(data => {
+      if (data.total == 0) {
+        iziToast.show({
+          title: 'Ops.',
+          titleColor: 'white',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: 'white',
+          color: 'red',
+          position: 'topCenter',
+          timeout: '5000',
+        });
+        return;
+      } else {
+        photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
+        if (photoGallery.children.length) {
+          buttonMore.hidden = false;
+        }
+        book.refresh();
+        page = page + 1;
+        if (page > pageLimit) {
+          iziToast.show({
+            titleColor: 'white',
+            message: `We're sorry, but you've reached the end of search results.!`,
+            messageColor: 'white',
+            color: 'blue',
+            position: 'topCenter',
+            timeout: '5000',
+          });
+          buttonMore.hidden = true;
+        }
+      }
+    })
+    .catch(error => {
       iziToast.show({
         title: 'Ops.',
         titleColor: 'white',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
+        message: error,
         messageColor: 'white',
         color: 'red',
         position: 'topCenter',
-        timeout: 5000,
+        timeout: '5000',
       });
-      return;
-    } else {
-      photoGallery.insertAdjacentHTML('beforeend', makeGallery(data.data));
-
-      if (photoGallery.children.length) {
-        buttonMore.hidden = false;
-      }
-
-      book.refresh();
-      page = page + 1;
-      checkShowMoreButton();
-    }
-  } catch (error) {
-    iziToast.show({
-      title: 'Ops.',
-      titleColor: 'white',
-      message: error,
-      messageColor: 'white',
-      color: 'red',
-      position: 'topCenter',
-      timeout: 5000,
+    })
+    .finally(() => {
+      loader.style.borderColor = 'white';
+      loader.style.borderBottomColor = 'transparent';
     });
-  }
-}
-
-function checkShowMoreButton() {
-  if (page >= pageLimit) {
-    iziToast.show({
-      titleColor: 'white',
-      message: `We're sorry, but you've reached the end of search results.!`,
-      messageColor: 'white',
-      color: 'blue',
-      position: 'topCenter',
-      timeout: 5000,
-    });
-    buttonMore.hidden = true;
-  }
 }
